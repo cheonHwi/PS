1e932285319f2f384fe3a56aca5245b1368afd47
cheonHwi <hoba0409@naver.com>
2023-11-08 23:14:19 +0900

    solved problem

--- programmers/kotlin/src/main/kotlin/similarityOfArray.kt
+++ programmers/kotlin/src/main/kotlin/similarityOfArray.kt
@@ -1,3 +1,23 @@
+// 배열의 유사도
+
+
+// ["a", "b", "c"]	["com", "b", "d", "p", "c"]	2
+// ["n", "omg"]	["m", "dot"]
+
+fun solution(s1: Array<String>, s2: Array<String>): Int {
+    var answer = 0
+    s1.forEach {
+        s2.forEach { s2 ->
+            if(it == s2) answer++
+        }
+    }
+    return answer
+}
+
 fun main() {
-    println("asdasf")
+    val s1 = arrayOf("a", "b", "c")
+    val s2 = arrayOf("com", "b", "d", "p", "c")
+
+    println(solution(s1, s2))
+
 }
